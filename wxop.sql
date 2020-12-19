e890ce611e18977fb62ee1e60bed1f8184538894
Pigee <haiou511@gmail.com>
2020-12-19 22:15:44 +0800

    Update wxop.sql

--- wxop.sql
+++ wxop.sql
@@ -11,3 +11,11 @@ from opid t1,tmeter t2
  where t1.clid = t2.clientid;
 
 update t1 set t1.cur_kh = t2.kh from opid t1,(select kh,wxid from opid where sn = 0) t2 where t1.wxid = t2.wxid;
+
+############
+
+update opid set kh = concat('32',right(concat('00000000',kh),9));
+update opid set cur_kh = concat('32',right(concat('00000000',cur_kh),9));
+
+insert into w_1592793652199(id,create_date,update_date,head,nickname,kh,cur_kh,hm,userbase_addr,openid)
+select replace(uuid(),'-',''),now(),now(),'e','e',kh,cur_kh,name,addr,wxid from opid;
