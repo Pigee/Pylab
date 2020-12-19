9ac619ab0a179f15b7e1b260e9ce43cab248624f
Pigee <haiou511@gmail.com>
2020-12-19 21:57:29 +0800

    Update opwxid.py
    last modified

--- opwxid.py
+++ opwxid.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
 
+#   使用说明  ./sxopid.py wxopid.txt result.txt
+#   第一个参数读入文件  第二个参数输出文件
+#   ！！！  最后一行clientid会少一个字符，，所以要手动加上  !!
+
 import sys
 
-file=open(sys.argv[1],mode='r')
+wfile=open(sys.argv[2],mode='a')
+
+sfile=open(sys.argv[1],mode='r')
 #  print(file)
-for line in file:
-   print(len(line))
-   sline=line[0:len(line)-2]
-   sstr =sline.split('，')
+for line in sfile:
+   #   print(len(line))
+   sline = line[0:len(line)-3]
+   print(sline)
+   sstr = sline.split(',')
    fele = sstr[0]
    i=0
    for ele in sstr[1:len(sstr)]:
-       print('insert into opid values('+'\''+fele+'\','+'\''+ele+'\','+str(i)+');')
+
+       wfile.write('insert into opid values(\''+fele+'\','+ele+','+str(i)+');\n')
        # print('\''+fele+'\'')
        # print('\''+ele+'\'')
        # print(i)
        i=i+1
+
+sfile.close()
+wfile.close()
